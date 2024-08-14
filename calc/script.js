55bb4ee86cb93985f30e476e22803045f1a8ac73
Harsha <saisri2946@gmail.com>
2024-08-14 10:13:39 +0530

    Completed calculator project

--- calc/script.js
+++ calc/script.js
@@ -1,15 +1,25 @@
 "use strict";
-function main() {
-    let number1 = null;
-    let number2 = null;
-    let operator = null;
-    let calculate = document.querySelector(".calculate");
-    let num1box = document.querySelector(".number1-container > .numberbox");
-    let num2box = document.querySelector(".number2-container > .numberbox");
-    calculate.addEventListener("click", (e) => {
-        number1 = num1box.getAttribute("value");
-        console.log(number1);
-    });
-}
-
-main();
+let number1cont = document.querySelector(".number1-container > .numberbox");
+let number2cont = document.querySelector(".number2-container > .numberbox");
+let operatorCont = document.querySelector(".operator > .operatorbox");
+let calculate = document.querySelector(".calculate-container > .calculate");
+let number1, number2, operator;
+calculate.addEventListener("click", () => {
+    number1 = number1cont.value;
+    number2 = number2cont.value;
+    operator = operatorCont.value;
+    switch(operator) {
+        case "*":
+            alert(Number(number1) * Number(number2));
+            break;
+        case "+":
+            alert(Number(number1) + Number(number2));
+            break;
+        case "-":
+            alert(Number(number1) - Number(number2));
+            break;
+        case "/":
+            alert(Number(number1) / Number(number2));
+            break;
+    }
+});
